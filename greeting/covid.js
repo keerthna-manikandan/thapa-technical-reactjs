ad6b73683974ecd80d34bf7a0188b20b958c7c39
keerthna-manikandan <67958625+keerthna-manikandan@users.noreply.github.com>
2021-06-24 11:10:25 +0530

    Add files via upload

---
+++ greeting/covid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Covid = () =>{
+    return(
+        <>
+            <h1>🔴 Live</h1>
+            <h2>COVID-19 CORONAVIRUS TRACKER </h2>
+        </>
+
+    )
+}
+
+export default Covid
